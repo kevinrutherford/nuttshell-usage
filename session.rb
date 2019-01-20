a6466cd2a3b9d984db0f9a5c1b3070935a52b165
Kevin Rutherford <kevin@rutherford-software.com>
2019-01-20 14:00:32 +0000

    Remove some unused code

--- session.rb
+++ session.rb
@@ -12,50 +12,8 @@ class Session
     @jwt = jwt
   end
 
-  def phase(title)
-    puts
-    puts ColorizedString[title].light_cyan
-  end
-
   def get(uri, expected_status=200)
     @api.send(:get, uri, nil, expected_status, @jwt)
-  end
-
-  def patch(uri, body, expected_status=200)
-    @api.send(:patch, uri, body, expected_status, @jwt)
-  end
-
-  def post(uri, body, expected_status=200)
-    @api.send(:post, uri, body, expected_status, @jwt)
-  end
-
-  def put(uri, body, expected_status=200)
-    @api.send(:put, uri, body, expected_status, @jwt)
-  end
-
-  def wait_for(uri, required_status=200)
-    response = nil
-    last_error = nil
-    for i in 1..5
-      begin
-        response = get(uri, required_status)
-        return response
-      rescue AcceptanceTestError => e
-        last_error = e
-        sleep 0.3
-      end
-    end
-    raise last_error if last_error
-    response
-  end
-
-  def wait_until(request)
-    for i in 1..5
-      response = get(request)
-      return response if yield response
-      sleep 0.3
-    end
-    raise AcceptanceTestError
   end
 
 end
--- usage.rb
+++ usage.rb
@@ -1,10 +1,7 @@
-# Copyright (C) 2018 Piford Software Limited - All Rights Reserved.
+# Copyright (C) 2019 Piford Software Limited - All Rights Reserved.
 # Unauthorized copying of this file, via any medium is strictly prohibited.
 # Proprietary and confidential.
 #
-
-require_relative '../migrations/api/server'
-require_relative '../migrations/domain/company'
 
 EXCLUDE = [
   'Adviser-Store',
@@ -13,33 +10,32 @@ EXCLUDE = [
   'Piford',
 ]
 
-Server.connect(silent: true).login do |session|
+key = ENV['API_KEY']
+hostname = "https://api.nuttshell.works"
+@api = Api.new(hostname, silent)
+session = Session.new(@api, key)
 
-  dashboard = session.get('/dashboard')
+companies = session.get('/companies')[:companies]
 
-  companies = session.get(dashboard[:actions][:companies])[:companies]
+data = companies.map do |co|
+  co = session.get(co[:actions][:self])
+  usage = session.get(co[:actions][:usage])
+  cols = usage[:months].map {|m| [m[:month], m[:nuttshells].length] }
+  months = {}
+  usage[:months].each {|m| months[m[:month]] = m[:nuttshells].length }
+  {
+    name: co[:name],
+    cols: cols,
+    months: months
+  }
+end.reject {|d| EXCLUDE.include?(d[:name]) }
 
-  data = companies.map do |co|
-    co = session.get(co[:actions][:self])
-    usage = session.get(co[:actions][:usage])
-    cols = usage[:months].map {|m| [m[:month], m[:nuttshells].length] }
-    months = {}
-    usage[:months].each {|m| months[m[:month]] = m[:nuttshells].length }
-    {
-      name: co[:name],
-      cols: cols,
-      months: months
-    }
-  end.reject {|d| EXCLUDE.include?(d[:name]) }
+months = data.map {|d| d[:cols].map {|c| c[0]} }.flatten.sort {|a,b| Time.parse(a) <=> Time.parse(b)}.uniq
 
-  months = data.map {|d| d[:cols].map {|c| c[0]} }.flatten.sort {|a,b| Time.parse(a) <=> Time.parse(b)}.uniq
+puts "Company,#{months.map {|m| m.gsub(/^(...).*(..)$/, "\\1 \\2")}.join(',')}"
 
-  puts "Company,#{months.map {|m| m.gsub(/^(...).*(..)$/, "\\1 \\2")}.join(',')}"
-
-  data.each do |co|
-    print "#{co[:name]},"
-    puts months.map {|m| co[:months].fetch(m, 0) }.join(',')
-  end
-
+data.each do |co|
+  print "#{co[:name]},"
+  puts months.map {|m| co[:months].fetch(m, 0) }.join(',')
 end
 
